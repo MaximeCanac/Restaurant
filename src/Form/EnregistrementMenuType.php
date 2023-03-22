f7dfc13b6d7adecb0cd0479a6997d028aed416f8
MaximeCanac <maxime.canac@e.rascol.net>
2023-03-22 10:47:01 +0100

    form menu

--- src/Form/EnregistrementMenuType.php
+++ src/Form/EnregistrementMenuType.php
@@ -1,28 +1,43 @@
 <?php
-
+// src/Form/EnregistrementMenuType.php
 namespace App\Form;
 
 use App\Entity\Plats;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class EnregistrementMenuType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('ingredient')
-            ->add('prix')
-            ->add('type')
+            ->add('entrees', ChoiceType::class, [
+                'choices' => $options['plats'],
+                'multiple' => true,
+                'expanded' => true,
+            ])
+            ->add('plats', ChoiceType::class, [
+                'choices' => $options['plats'],
+                'multiple' => true,
+                'expanded' => true,
+            ])
+            ->add('desserts', ChoiceType::class, [
+                'choices' => $options['plats'],
+                'multiple' => true,
+                'expanded' => true,
+            ])
+            ->add('submit', SubmitType::class, ['label' => 'Afficher les plats'])
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => Plats::class,
+            'plats' => [],
         ]);
     }
 }
