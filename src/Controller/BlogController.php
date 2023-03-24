42b71a95987b7bfbeff3bff8b22c0859f7448767
tommylourenco <tommy.lourenco@e.rascol.net>
2023-03-24 14:44:59 +0100

    creation blog

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,17 +2,47 @@
 
 namespace App\Controller;
 
+use App\Entity\Blog;
+use App\Entity\Post;
+use App\Form\BlogType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'app_blog')]
-    public function index(): Response
+    /**
+     * @Route("/blog/{postId}", name="blog_show")
+     */
+    public function show($postId, Request $request)
     {
-        return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+        $entityManager = $this->getDoctrine()->getManager();
+        $post = $entityManager->getRepository(Post::class)->find($postId);
+
+        if (!$post) {
+            throw $this->createNotFoundException('The post does not exist');
+        }
+
+        $blog = new Blog();
+        $blog->setPost($post);
+
+        $form = $this->createForm(BlogType::class, $blog);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($blog);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('blog_show', ['postId' => $postId]);
+        }
+
+        $blogEntries = $entityManager->getRepository(Blog::class)->findBy(['post' => $post]);
+
+        return $this->render('blog/show.html.twig', [
+            'post' => $post,
+            'blogEntries' => $blogEntries,
+            'form' => $form->createView(),
         ]);
     }
 }
