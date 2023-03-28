6664708b4d95655dd6727cce14c5a003fb1bd6fc
MaximeCanac <maxime.canac@e.rascol.net>
2023-03-28 17:15:20 +0200

    menu test 154546456 :(

--- src/Form/PlatsFormType.php
+++ src/Form/PlatsFormType.php
@@ -1,45 +1,63 @@
 <?php
+// src/Form/SelectionType.php
 
 namespace App\Form;
+namespace App\Repository;
 
 use App\Entity\Plats;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class PlatsFormType extends AbstractType
+class SelectionType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('entrees', EntityType::class, [
                 'class' => Plats::class,
-                'choices' => $options['plats'],
+                'query_builder' => function (PlatsRepository $repo) {
+                    return $repo->createQueryBuilder('p')
+                        ->andWhere('p.type = :type')
+                        ->setParameter('type', 'entree')
+                        ->orderBy('p.nom', 'ASC');
+                },
                 'multiple' => true,
                 'expanded' => true,
-                'label' => 'Choisissez 3 entrées'
             ])
             ->add('plats', EntityType::class, [
                 'class' => Plats::class,
-                'choices' => $options['plats'],
+                'query_builder' => function (PlatsRepository $repo) {
+                    return $repo->createQueryBuilder('p')
+                        ->andWhere('p.type = :type')
+                        ->setParameter('type', 'plat')
+                        ->orderBy('p.nom', 'ASC');
+                },
                 'multiple' => true,
                 'expanded' => true,
-                'label' => 'Choisissez 3 plats'
             ])
             ->add('desserts', EntityType::class, [
                 'class' => Plats::class,
-                'choices' => $options['plats'],
+                'query_builder' => function (PlatsRepository $repo) {
+                    return $repo->createQueryBuilder('p')
+                        ->andWhere('p.type = :type')
+                        ->setParameter('type', 'dessert')
+                        ->orderBy('p.nom', 'ASC');
+                },
                 'multiple' => true,
                 'expanded' => true,
-                'label' => 'Choisissez 3 desserts'
-            ]);
+            ])
+            ->add('submit', SubmitType::class, ['label' => 'Sélectionner'])
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setRequired(['plats']);
-        $resolver->setAllowedTypes('plats', 'array');
+        $resolver->setDefaults([
+            // configure your form options here
+        ]);
     }
 }
