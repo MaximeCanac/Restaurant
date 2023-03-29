3139a3c82ccc1755ba4cd228b5652d06626024e9
MaximeCanac <maxime.canac@e.rascol.net>
2023-03-29 11:56:14 +0200

    menu base selection

--- src/Controller/SelectionController.php
+++ src/Controller/SelectionController.php
@@ -4,17 +4,19 @@
 namespace App\Controller;
 
 use App\Entity\Plats;
+use App\Entity\Selection;
 use App\Form\SelectionType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class SelectionController extends AbstractController
 {
     
     #[Route('/selection', name: 'selection')]
-    public function selection(Request $request): Response
+    public function selection(Request $request, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(SelectionType::class);
         $form->handleRequest($request);
@@ -23,6 +25,16 @@ class SelectionController extends AbstractController
             $entrees = $form->get('entrees')->getData();
             $plats = $form->get('plats')->getData();
             $desserts = $form->get('desserts')->getData();
+
+// Créez une instance de l'entité Selection et définit ses propriétés
+            $selection = new Selection();
+            $selection->setEntrees($entrees);
+            $selection->setPlats($plats);
+            $selection->setDesserts($desserts);
+
+ // Persiste l'objet dans la base de données
+            $entityManager->persist($selection);
+            $entityManager->flush();
 
             return $this->render('selection/selection.html.twig', [
                 
