8be915b6fb3e7ad8b6bab97c16242afe3179f7a6
MaximeCanac <maxime.canac@e.rascol.net>
2023-05-09 08:50:15 +0200

    syncro

---
+++ src/Entity/Avis.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\AvisRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AvisRepository::class)]
+#[ApiResource]
+class Avis
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $commentaire = null;
+
+    #[ORM\Column]
+    private ?int $note = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCommentaire(): ?string
+    {
+        return $this->commentaire;
+    }
+
+    public function setCommentaire(?string $commentaire): self
+    {
+        $this->commentaire = $commentaire;
+
+        return $this;
+    }
+
+    public function getNote(): ?int
+    {
+        return $this->note;
+    }
+
+    public function setNote(int $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+}
