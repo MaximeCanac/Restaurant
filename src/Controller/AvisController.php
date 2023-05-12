815a0d54fb336ebbcfb5b8651e3f036651c29d63
MaximeCanac <maxime.canac@e.rascol.net>
2023-05-12 12:02:36 +0200

    api

--- src/Controller/AvisController.php
+++ src/Controller/AvisController.php
@@ -1,18 +1,23 @@
 <?php
+// src/Controller/AvisController.php
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\AvisRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AvisController extends AbstractController
 {
-    #[Route('/avis', name: 'app_avis')]
-    public function index(): Response
+    #[Route('/avis', name: 'avis')]
+    public function index(AvisRepository $avisRepository): Response
     {
+        $avis = $avisRepository->findAll();
+
         return $this->render('avis/index.html.twig', [
-            'controller_name' => 'AvisController',
+            'avis' => $avis,
         ]);
     }
 }
+
