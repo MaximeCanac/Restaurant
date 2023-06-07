b239e171653b475ecaadcaf73af7d95a556fb080
MaximeCanac <maxime.canac@e.rascol.net>
2023-06-07 10:20:00 +0200

    image

--- src/Form/MenuType.php
+++ src/Form/MenuType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class MenuType extends AbstractType
 {
